8f3e50452e47f9e5bd20820be791b45a767c26e6
Cory Spitzer <coryspitzer@gmail.com>
2016-02-20 09:15:00 -0500

    Say hello

---
+++ greeter.py
@@ -0,0 +1,9 @@
+
+def main():
+    name = raw_input("What is your name? ")
+    if name.lower() == "cory":
+        print "Hello dear Cory!"
+    else:
+        print "Hello " + name + "!"
+
+main()
